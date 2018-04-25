992e95ed0f1a5674b5d1bd787214930790e6ac3e
cielzhao <1425066368@qq.com>
2018-04-25 18:11:15 +0800

    add new files

---
+++ js/common/footer.js
@@ -0,0 +1,1 @@
+let footer = `<p>Copyright © 2018 Ciel. All rights reserved.</p>`;
---
+++ js/common/header.js
@@ -0,0 +1,49 @@
+let header =
+`<div class="container">
+	<nav class="navbar navbar-inverse" role="navigation">
+	    <div class="navbar-header">
+	        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	            <span class="sr-only">切换导航</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	        </button>
+	        <a class="navbar-brand" href="#">Ciel</a>
+	    </div>
+
+	    <div class="navbar-collapse collapse">
+	        <ul class="nav navbar-nav">
+	            <li>
+	            	<a href="index.html" class="index current">
+	            		<span>首页</span>
+	            		<span class="en">Home</span>
+	            	</a>
+	            </li>
+	            <li>
+	            	<a href="about.html" class="about">
+	            		<span>关于我</span>
+	            		<span class="en">About</span>
+	            	</a>
+	            </li>
+	            <li>
+	            	<a href="http://cielzhao.com/">
+	            		<span>博客</span>
+	            		<span class="en">Blog</span>
+	            	</a>
+	            </li>
+	            <li>
+	            	<a href="album.html" class="album">
+	            		<span>相册</span>
+	            		<span class="en">album</span>
+	            	</a>
+	            </li>
+	            <li>
+	            	<a href="life.html" class="life">
+	            		<span>慢生活</span>
+	            		<span class="en">Life</span>
+	            	</a>
+	            </li>
+	        </ul>
+	    </div>
+	</nav>
+</div>`;
