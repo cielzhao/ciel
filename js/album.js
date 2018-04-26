a574a040ad00735216f8cc7fc55924d6461ce3ed
cielzhao <1425066368@qq.com>
2018-04-26 23:28:13 +0800

    update files

--- js/album.js
+++ js/album.js
@@ -36,44 +36,6 @@
 
 	};
 
-	// Animations
-
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight animated');
-							} else {
-								el.addClass('fadeInUp animated');
-							}
-
-							el.removeClass('item-animate');
-						},  k * 200, 'easeInOutExpo' );
-					});
-
-				}, 100);
-
-			}
-
-		} , { offset: '85%' } );
-	};
-
 	// Document on load.
 	$(function(){
 		fullHeight();
--- js/common.js
+++ js/common.js
@@ -11,7 +11,6 @@ function init() {
 	$('.' + currentClass).addClass('current');
 }
 
-
 //获取当前页面html名称
 function getPageName() {
 	var urlItem = location.href;
@@ -25,3 +24,32 @@ function getPageName() {
 	}
 	return filename;
 }
+
+//页面内容进入动画
+function contentWayPoint() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+				i++;
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn animated');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft animated');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight animated');
+							} else {
+								el.addClass('fadeInUp animated');
+							}
+							el.removeClass('item-animate');
+						},  k * 200, 'easeInOutExpo' );
+					});
+				}, 100);
+			}
+		} , { offset: '85%' } );
+	};
